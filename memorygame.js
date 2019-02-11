350863f9d71a024d3d177d410af0c39b3efdda08
Małgorzata Burchard <pilatmalgo@gmail.com>
2019-02-11 17:38:41 +0100

    memory js

--- memorygame.js
+++ memorygame.js
@@ -1,16 +1,8 @@
-const generateTable = function(n, m){
-    for (i = 0; i< n*m; i++){
-        //console.log ("dodaje div");
-        var div = document.createElement("div");
-        document.getElementById ("main").appendChild(div);
-    }
-}
+const cardsColor = ["purple", "yellow", 
+"green", "white","navy","red", 
+"magenta","cyan", "orange"];
 
-const cardsColor = ["purple", "purple", "yellow", "yellow", 
-"green", "green", "white", "white", "navy", "navy", "red", "red", 
-"magenta", "magenta", "cyan", "cyan", "orange", "orange"];
-
-
+let cardsNumber;
 
 let cards = "";
 
@@ -22,12 +14,78 @@ let activeCard = "";
 //zmienna przechowująca parę kart będąca tablicą
 const activeCards = []; 
 
-
 let gameResult = 0;
+
+//tworze funkcje nadajace kolory wywolanym divom
+const init = function(x){
+    cardsNumber = x;
+    const $container = document.getElementById ("main")
+    const chosenCards = chooseCards(shuffle(cardsColor), cardsNumber);
+    const finalChosen = shuffle([...chosenCards, ...chosenCards])
+    finalChosen.forEach((currentElement ) => {
+        generateCard(currentElement, $container);
+    })
+    $container.addEventListener('click', boardClickManager);
+
+
+    // generateTable(3,6);
+    // cards = document.querySelectorAll ("div");
+    // cards = [...cards]; //tablica z wszystkimi divami
+    // //cards = [rozwija listę osiemnastu div i zamienia je na tablice]
+    // gamePairs = cards.length/2;
+
+    // cards.forEach(card =>{
+
+    // })
+
+    //  setTimeout(function(){
+    //      cards.forEach(card =>{
+    //        card.classList.add("hidden")
+    //       card.addEventListener("click", clickCard)
+    //      })
+    //  }, 2000)
+}
+
+function boardClickManager(event) {
+
+    if(event.target.classList.contains('matched') || "visible"){
+        return;
+    } else {
+        console.log(event);
+    }
+}
+
+function generateCard(card, container) {
+    var div = document.createElement("div");
+    div.classList.add(card);
+
+    container.appendChild(div);
+}
+
+const generateTable = function(n, m){
+    var cos = document.getElementById ("main");
+    for (i = 0; i< n*m; i++){
+        //console.log ("dodaje div");
+        var div = document.createElement("div");
+        cos.appendChild(div);
+    }
+}
+
+function chooseCards(array, number) {
+    return array.splice(0, number);
+}
+
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
 
 //funkcja clickCard, która będzie uruchamiana każdorazowo 
  //po kliknięciu w jeden kwadrat
-const clickCard = function() {
+ const clickCard = function() {
     activeCard = this;
 
     if (activeCard == activeCards[0]) return;
@@ -40,7 +98,6 @@ if(activeCards.length === 0) {
         console.log("1");
         return;
 }
-
 //jeśli drugie klikniecie
 else {
     console.log("2");
@@ -48,58 +105,32 @@ else {
     activeCards[1] = activeCard;
     //console.log(activeCards)
     
-    setTimeout(function (){
-
-        if(activeCards[0].className === activeCards[1].className ){
-            console.log('wygrana')
-            activeCards.forEach(card => card.classList.add("off"))
-            gameResult++;
-            cards = cards.filter(card => !card.classList.contains("off"));
+setTimeout(function (){
+    if(activeCards[0].className === activeCards[1].className ){
+        console.log('wygrana')
+        activeCards.forEach(card => card.classList.add("off"))
+        gameResult++;
+        cards = cards.filter(card => !card.classList.contains("off"));
             if(gameResult == gamePairs) {
                 const endTime = new Date().getTime();
                 const gameTime = (endTime - startTime)/1000
                 alert(`Brawo! Twój wynik to: ${gameTime} sekund`)
                 location.reload();
             } 
-        }
-
-        else{
-            console.log("przegrana")
-            activeCards.forEach(card => card.classList.add("hidden"))
+     }
+    else{
+        console.log("przegrana")
+        activeCards.forEach(card => card.classList.add("hidden"))
         }
         activeCard ="";
         activeCards.length = 0;
         cards.forEach(card => card.addEventListener("click", clickCard))
     }, 500)
-
     }
     
-
 };
-//tworze funkcje nadajace kolory wywolanym divom
-const init = function(){
-    generateTable(3,6);
-    cards = document.querySelectorAll ("div");
-    cards = [...cards]; //tablica z wszystkimi divami
-    //cards = [rozwija listę osiemnastu div i zamienia je na tablice]
-    gamePairs = cards.length/2;
-
-    cards.forEach(card =>{
-        const position = Math.floor(Math.random() *
-        cardsColor.length); //wulosuje 0,12,3,4,...17
-        card.classList.add(cardsColor[position]);
-        cardsColor.splice(position,1);
-    })
-
-    setTimeout(function(){
-        cards.forEach(card =>{
-            card.classList.add("hidden")
-            card.addEventListener("click", clickCard)
-        })
-    }, 2000)
-}
 
 window.onload = function () {
-    init();
+    init(20);
 };
 
