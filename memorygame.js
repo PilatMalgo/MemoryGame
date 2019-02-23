d6e630c951f8053402044aa10e3226bb2aaacbb8
Małgorzata Burchard <pilatmalgo@gmail.com>
2019-02-23 23:43:20 +0100

    game over

--- memorygame.js
+++ memorygame.js
@@ -2,9 +2,9 @@ const cardsColor = ["purple", "yellow",
 "green", "white","navy","red", 
 "magenta","cyan", "orange"];
 
-let cardsNumber;
+let cardsNumber; 
 
-let cards = "";
+let cards = ""; 
 
 const startTime = new Date().getTime();
 //console.log(startTime)
@@ -18,85 +18,76 @@ let gameResult = 0;
 
 //tworze funkcje nadajace kolory wywolanym divom
 const init = function(x){
-    cardsNumber = x;
-    const $container = document.getElementById ("main")
+    cardsNumber = x; 
+    const $container = document.getElementById("main");
     const chosenCards = chooseCards(shuffle(cardsColor), cardsNumber);
     const finalChosen = shuffle([...chosenCards, ...chosenCards])
     finalChosen.forEach((currentElement ) => {
         generateCard(currentElement, $container);
     })
     $container.addEventListener('click', boardClickManager);
-
-
-    // generateTable(3,6);
-    // cards = document.querySelectorAll ("div");
-    // cards = [...cards]; //tablica z wszystkimi divami
-    // //cards = [rozwija listę osiemnastu div i zamienia je na tablice]
-    // gamePairs = cards.length/2;
-
-    // cards.forEach(card =>{
-
-    // })
-
-    //  setTimeout(function(){
-    //      cards.forEach(card =>{
-    //        card.classList.add("hidden")
-    //       card.addEventListener("click", clickCard)
-    //      })
-    //  }, 2000)
 }
 //funkcja clickCard, która będzie uruchamiana każdorazowo 
  //po kliknięciu w jeden kwadrat
 function clickCard(activeCard) {
-	console.log("clickCard");
-		//if (activeCard == activeCards[0]) return;
 
-	activeCard.classList.remove("hidden");
+	if (!activeCard.classList.contains("off")){
+		console.log("clickCard");
+			//if (activeCard == activeCards[0]) return;
 
-	console.log(activeCard);
-	//jeśli pierwsze klikniecie
-	if(activeCards.length === 0) {
-			activeCards [0] = activeCard;
-			console.log("1");
-			return;
-	}
-	//jeśli drugie klikniecie
-	else {
-		console.log("2");
-		cards.forEach(card => card.removeEventListener("click", clickCard))
-		activeCards[1] = activeCard;
-		//console.log(activeCards)
+		activeCard.classList.remove("hidden");
+
+		console.log(activeCard);
+		//jeśli pierwsze klikniecie
+	
+		if(activeCards.length === 0) {
+				activeCards [0] = activeCard;
+				console.log("1");
+				return;
+		}
+		//jeśli drugie klikniecie
+		else {
+			
+			console.log("2");
+			document.getElementById("main").removeEventListener("click", clickCard);
+			activeCards[1] = activeCard;
+			//console.log(activeCards)
 		
-	setTimeout(function (){
-		if(activeCards[0].className === activeCards[1].className ){
-			console.log('wygrana')
-			activeCards.forEach(card => card.classList.add("off"))
-			gameResult++;
-			cards = cards.filter(card => !card.classList.contains("off"));
-				if(gameResult == gamePairs) {
-					const endTime = new Date().getTime();
-					const gameTime = (endTime - startTime)/1000
-					alert(`Brawo! Twój wynik to: ${gameTime} sekund`)
-					location.reload();
-				} 
-		 }
-		else{
-			console.log("przegrana")
-			activeCards.forEach(card => card.classList.add("hidden"))
-			}
-			activeCard ="";
-			activeCards.length = 0;
-			cards.forEach(card => card.addEventListener("click", clickCard))
-		}, 500)
+			setTimeout(function (){
+				if(activeCards[0].className === activeCards[1].className ){
+					console.log('wygrana');
+					
+					activeCards.forEach(card => card.classList.add("off"))
+					gameResult++;
+
+					if (gameResult == cardsNumber) { 
+						const endTime = new Date().getTime();
+						const gameTime = (endTime - startTime) / 1000
+						alert(`Great Job! Time: ${gameTime} sec`)
+						//location.reload();
+					}
+				 }
+				 
+				else{
+					console.log("przegrana")
+					activeCards.forEach(card => card.classList.add("hidden"));
+				}
+					activeCards.forEach(card => card.classList.remove("visible"));
+					activeCard ="";
+					activeCards.length = 0;
+					document.getElementById("main").addEventListener('click', boardClickManager);
+				}, 500)
 		}
 		
+	}		
 };
 
 function boardClickManager(event) {
-    if(event.target.classList.contains('matched') || event.target.classList.contains('visible') ){
-		console.log("aaa");
+    if(event.target.classList.contains('visible') || event.target.classList.contains('off') || activeCards.length > 1 ){
+		console.log("active cards:"+activeCards.length);
         return;
     } else {
+		event.target.classList.add("visible");
 		clickCard(event.target);
     }
 }
@@ -131,44 +122,42 @@ function shuffle(a) {
     return a;
 }
 
-
-
-
-window.onload = function () {
-	
-	//listener dla buttona menu
-document.getElementById("start_game").addEventListener("click", function(){
+function start_game(e){
   //pobieram level z selecta w HTML
   var sel = document.getElementById("game_level");
   var level = sel.options[sel.selectedIndex];
-  
-//odpalam gre z wybranym poziomem
+	  
+	//odpalam gre z wybranym poziomem
 
-var cardsNumber = 6;
-console.log(level.value);
+	var cardsNumber = 6;
+	console.log(level.value);
 
-if (level.value == "easy"){
-	cardsNumber = 6;
-	console.log(level.value);
+	if (level.value == "easy"){
+		cardsNumber = 3;
+		console.log(level.value);
+	}
+	else if (level.value == "medium") {
+		cardsNumber = 6;
+		console.log(level.value);
+	}
+	else if (level.value == "hard") {
+		cardsNumber = 9;
+		console.log(level.value);
+	}
+	 init(cardsNumber);
+	  
+	 
+	//chowam menu 
+	  var menu = document.getElementById("menu");
+	  menu.style.display = 'none';
 }
-else if (level.value == "medium") {
-	cardsNumber = 12;
-	console.log(level.value);
-}
-else if (level.value == "hard") {
-	cardsNumber = 18;
-	console.log(level.value);
-}
- init(cardsNumber);
-  
- 
-//chowam menu 
-  var menu = document.getElementById("menu");
-  menu.style.display = 'none';
-  
-});
 
-	
-    
+
+
+window.onload = function () {	
+	//listener dla buttona menu
+	document.getElementById("start_game").addEventListener("click", function(){
+		start_game();
+	});
 };
 
